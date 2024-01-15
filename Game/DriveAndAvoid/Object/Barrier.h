65e2b65a3187d4e4b6513c0118ac754b911e2110
4S4T <138412409+4S4T@users.noreply.github.com>
2024-01-15 18:00:44 +0900

    1/15

--- Game/DriveAndAvoid/Object/Barrier.h
+++ Game/DriveAndAvoid/Object/Barrier.h
@@ -1,1 +1,24 @@
 #pragma once
+
+#include"../Utility/Vector2D.h"
+
+class Barrier
+{
+private:
+
+	//バリア画像
+	int image;
+
+	//バリアの寿命
+	float life_span;
+
+public:
+	Barrier();
+	~Barrier();
+
+	//画像処理
+	void Draw(const Vector2D& location);
+
+	//寿命が尽きたか？
+	bool IsFinished(float speed);
+};
